2fb2690accbf9529f1d64fe9983918b109e9b41d
Ghanshyam Gupta <ghanshyamg.cs.22@nitj.ac.in>
2024-11-22 03:12:31 +0530

    Auth error corrected

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,24 +2,26 @@ const jwt = require("jsonwebtoken");
 const User = require("./../models/users");
 
 const authenticate = async (req, res, next) => {
-  try {
-    const token = req.cookies.jwtoken;
+    try{
+        const token = req.cookies.jwtoken;
+        
+        if(token === undefined){
+            res.redirect("/auth");
+        }
+        else{
+            const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+            const user = await User.findOne({ _id: verifyToken.user._id });
 
-    if (token === undefined) {
-      res.redirect("/auth");
-    } else {
-      const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-      const user = await User.findOne({ _id: verifyToken.user._id });
+            if(!user){
+                throw new Error("You don't have admin rights");
+            }
 
-      if (!user) {
-        throw new Error("You don't have admin rights");
-      }
-
-      next();
+            next();
+        }
     }
-  } catch (err) {
-    res.status(400).send("No token");
-  }
-};
+    catch(err){
+        res.status(400).send("No token");
+    }
+}
 
 module.exports = authenticate;
